ae0c165fa7bac4fbafaa9798e4207554edc8a6a1
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-10-17 21:21:29 -0400

    Added Volunteer model and Token Model

---
+++ Replate/Replate/Models/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  Replate
+//
+//  Created by Michael Stoffer on 10/17/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+
+struct Token: Codable {
+    let token: String
+}
