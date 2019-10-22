8dcee986177e9d50ea1724dd9457c6c101ffefa1
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-10-21 22:21:47 -0400

    Fixed bug with duplicate enums

--- Replate/Replate/Model Controllers/APIClient.swift
+++ Replate/Replate/Model Controllers/APIClient.swift
@@ -11,6 +11,8 @@ import Foundation
 enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
 }
 
 enum NetworkError: Error {
--- Replate/Replate/Model Controllers/FoodController.swift
+++ Replate/Replate/Model Controllers/FoodController.swift
@@ -8,21 +8,6 @@
 
 import Foundation
 
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-}
-
-enum NetworkError: Error {
-    case noAuth
-    case badAuth
-    case otherError
-    case badData
-    case noDecode
-}
-
 class FoodController {
     
     // MARK: Properties
