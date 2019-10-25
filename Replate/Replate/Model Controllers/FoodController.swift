d45caf2d3383dd5334f5b05fd7c54eaf5be84eea
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-10-24 23:03:35 -0400

    Updated CreateDonation func

--- Replate/Replate/Model Controllers/FoodController.swift
+++ Replate/Replate/Model Controllers/FoodController.swift
@@ -55,39 +55,34 @@ class FoodController {
     }
     
     // User can create a new donation
-    func createDonation(with donation: Food, completion: @escaping (Result<Food, NetworkError>) -> Void) {
+    func createDonation(withName name: String, withPickupDate pickupDate: String, withTime time: String, withDescription description: String, completion: @escaping (Result<Food, NetworkError>) -> Void) {
         var request = URLRequest(url: baseURL)
         request.httpMethod = HTTPMethod.post.rawValue
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue(LoginController.shared.token!.token, forHTTPHeaderField: "Authorization")
-        
+
         let jsonEncoder = JSONEncoder()
         jsonEncoder.dateEncodingStrategy = .iso8601
         do {
-            
-            let jsonData = try jsonEncoder.encode(donation)
-            request.httpBody = jsonData
-            print(donation)
+            let userParams = ["name": name, "time": time, "description": description, "is_claimed": 0, "pickup_date": pickupDate] as [String: Any]
+            let json = try JSONSerialization.data(withJSONObject: userParams, options: .prettyPrinted)
+            request.httpBody = json
         } catch {
             print("Error encoding gig object: \(error.localizedDescription)")
             completion(.failure(.otherError))
             return
         }
-        
+
         URLSession.shared.dataTask(with: request) { (_, response, error) in
             if let response = response as? HTTPURLResponse,
                 response.statusCode == 401 {
                 completion(.failure(.badAuth))
             }
-            
+
             if let _ = error {
                 completion(.failure(.otherError))
                 return
             }
-            
-            self.donations.append(donation)
-            completion(.success(donation))
-            
         }.resume()
     }
     
